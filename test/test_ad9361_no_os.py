35d379dfa510b06a0bf58f6a8ae7543324f2385b
Trecia Agoylo <trecia.agoylo@analog.com>
2022-07-06 14:58:30 +0800

    debug

--- test/test_ad9361_no_os.py
+++ test/test_ad9361_no_os.py
@@ -147,15 +147,15 @@ params = dict(
 
 
 # #########################################
-# @pytest.mark.iio_hardware(hardware, True)
-# @pytest.mark.parametrize("classname", [(classname)])
-# @pytest.mark.parametrize("channel", [0, 1, [0, 1]])
-# def test_ad9361_rx_data(test_dma_rx, iio_uri, classname, channel):
-#     test_dma_rx(iio_uri, classname, channel)
+@pytest.mark.iio_hardware(hardware, True)
+@pytest.mark.parametrize("classname", [(classname)])
+@pytest.mark.parametrize("channel", [0, 1, [0, 1]])
+def test_ad9361_rx_data(test_dma_rx, iio_uri, classname, channel):
+    test_dma_rx(iio_uri, classname, channel)
 
 
 #########################################
-@pytest.mark.iio_hardware(hardware)
+@pytest.mark.iio_hardware(hardware, True)
 @pytest.mark.parametrize("classname", [(classname)])
 @pytest.mark.parametrize("channel", [0, 1, [0, 1]])
 def test_ad9361_tx_data(test_dma_tx, iio_uri, classname, channel):
